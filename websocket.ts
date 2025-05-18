999cf07b54aadb7e86aef23ea553fcbb77fe9ef8
rityjacob <rityjacob@gmail.com>
2025-05-17 21:07:13 -0400

    Added Account Subscription

--- websocket.ts
+++ websocket.ts
@@ -1,13 +1,32 @@
-import { WebSocket } from "ws";
+import WebSocket from "ws";
+
+function subscribeToAccount(ws: WebSocket, account: string){
+  const requestdata= {
+    "jsonrpc": "2.0",
+    "id": 1903,
+    "method": "accountSubscribe",
+    "params": [
+      account,
+      {
+        "encoding": "jsonParsed",
+        "commitment": "confirmed"
+      }
+    ]
+  };
+  ws.send(JSON.stringify(requestdata))
+}
 
 const ws = new WebSocket("wss://api.devnet.solana.com/");
 
 ws.on("open", () => {
   console.log("new connection!");
+
+  subscribeToAccount(ws, "3qBKimmvwd2HaXCwemH62SDqgWtXXcZ2q4321SEWBSw5")
 });
 
-ws.on("message", (data: WebSocket.Data) => {
-  console.log(data);
+ws.on("message", (data) => {
+  console.log("New message:");
+  console.log(data.toString());
 });
 
 ws.on("error", (error: Error) => {
@@ -16,5 +35,5 @@ ws.on("error", (error: Error) => {
 });
 
 ws.on("close", () => {
-  console.log("connection closed");
+  console.log("connection closed! Bye!");
 });
