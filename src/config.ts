356fd387d1d7956d1006c4f6c31bcd6f5a24cfcc
rityjacob <rityjacob@gmail.com>
2025-05-27 13:18:11 -0400

    Updated jupiter api url

--- src/config.ts
+++ src/config.ts
@@ -53,7 +53,7 @@ export const TRANSACTION_CONFIG: TransactionConfig = {
 };
 
 export const DEX_CONFIG: DexConfig = {
-    jupiterApiUrl: 'https://quote-api.jup.ag/v6',
+    jupiterApiUrl: 'https://quote-api.jup.ag',
     minLiquidity: 2000,
     maxPriceImpact: 2,
     trustedDexes: ['RAYDIUM','ORCA','JUPITER']
