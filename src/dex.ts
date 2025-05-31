8d833da4d2184291d86239465bc8f0cf451c4d02
rityjacob <rityjacob@gmail.com>
2025-05-31 00:39:02 -0400

    Price api fixec

--- src/dex.ts
+++ src/dex.ts
@@ -68,7 +68,7 @@ class DexManager {
     async getTokenPrice(tokenAddress: string): Promise<number> {
         try {
             const response = await this.rateLimitedFetch(
-                `https://lite-api.jup.ag/price/v2/price?ids=${tokenAddress}`,
+                `https://lite-api.jup.ag/price/v2?ids=${tokenAddress}`,
                 {
                     method: 'GET',
                     headers: { 'Content-Type': 'application/json' }
